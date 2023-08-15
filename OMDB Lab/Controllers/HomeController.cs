642a2d76eda42aff4113753f2a8cce4f5cb9ef0d
joseph heath <heathj873@gmail.com>
2023-08-15 18:02:46 -0400

    first

---
+++ OMDB Lab/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OMDB_Lab.Models;
+using System.Diagnostics;
+
+namespace OMDB_Lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieSearchForm()
+        {
+            
+            return View("MovieSearch");
+        }
+
+        [HttpPost]
+        public IActionResult MovieSearchResults(string title)
+        {
+            SingleMovieModel result = SingleMovieDAL.GetMovie(title);
+
+            return View("MovieSearch", result);
+        }
+
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MovieNightForm()
+        {
+            return View("MovieNight");
+        }
+        [HttpPost]
+        public IActionResult MovieNightResults(string title1, string title2, string title3)
+        {
+            List<SingleMovieModel> result = new List<SingleMovieModel>();
+
+            SingleMovieModel result1 = SingleMovieDAL.GetMovie(title1);
+            result.Add(result1);
+            SingleMovieModel result2 = SingleMovieDAL.GetMovie(title2);
+            result.Add(result2);
+            SingleMovieModel result3 = SingleMovieDAL.GetMovie(title3);
+            result.Add(result3);
+
+
+
+            return View("MovieNight", result);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OMDB Lab/Models/SingleMovieDAL.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace OMDB_Lab.Models
+{
+    public class SingleMovieDAL
+    {
+        public static SingleMovieModel GetMovie(string title)
+        {
+            //Setup
+            string url = $"http://www.omdbapi.com/?i=tt3896198&apikey=b8fb13c1&t={title}";
+
+
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Convert to c#
+            SingleMovieModel result = JsonConvert.DeserializeObject<SingleMovieModel>(JSON);
+            return result;
+        }
+    }
+}
